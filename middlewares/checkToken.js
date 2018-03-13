0d4bf21b4019321d8dd8fe31047fabf1040a612c
Alexander <vaeum@mail.ru>
2018-03-13 22:13:52 +0300

    add checkToken middleware

---
+++ middlewares/checkToken.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+
+const checkToken = async (req, res, next) => {
+  const token = req.headers['Authorization']
+
+  if (!token) {
+    res.status(403).json({ message: 'Forbidden. No Token !' })
+    return
+  }
+
+  try {
+    const tokenObj = jwt.verify(token, process.env.NODE_JWT_SECRET)
+  } catch ({ message }) {
+    res.status(403).json({ message })
+    return
+  }
+
+  req.token = tokenObj
+  next()
+}
+
+module.exports = checkToken
